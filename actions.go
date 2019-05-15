dad51aad5a16a53dc836db44376970f3b8877727
Paula Altolaguirre <paltolaguirre@finnegans.com.ar>
2019-05-15 12:31:58 -0300

    cambios para que utilice las func comunesdeclaradas en las librerias

--- actions.go
+++ actions.go
@@ -2,29 +2,28 @@ package main
 
 import (
 	"encoding/json"
-	"io/ioutil"
 	"net/http"
 	"strconv"
 
 	"github.com/gorilla/mux"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
-	"github.com/xubiosueldos/autenticacion/publico"
-	"github.com/xubiosueldos/conexionBD"
+	"github.com/xubiosueldos/autenticacion/apiclientautenticacion"
+	"github.com/xubiosueldos/conexionBD/apiclientconexionbd"
 	"github.com/xubiosueldos/framework"
 	"github.com/xubiosueldos/novedad/structNovedad"
 )
 
 func NovedadList(w http.ResponseWriter, r *http.Request) {
 
-	tokenAutenticacion, tokenError := checkTokenValido(r)
+	tokenAutenticacion, tokenError := apiclientautenticacion.CheckTokenValido(r)
 
 	if tokenError != nil {
-		errorToken(w, tokenError)
+		apiclientautenticacion.ErrorToken(w, tokenError)
 		return
 	} else {
 
-		db := obtenerDB(tokenAutenticacion)
+		db := apiclientconexionbd.ObtenerDB(tokenAutenticacion)
 		automigrateTablasPrivadas(db)
 		defer db.Close()
 
@@ -39,10 +38,10 @@ func NovedadList(w http.ResponseWriter, r *http.Request) {
 
 func NovedadShow(w http.ResponseWriter, r *http.Request) {
 
-	tokenAutenticacion, tokenError := checkTokenValido(r)
+	tokenAutenticacion, tokenError := apiclientautenticacion.CheckTokenValido(r)
 
 	if tokenError != nil {
-		errorToken(w, tokenError)
+		apiclientautenticacion.ErrorToken(w, tokenError)
 		return
 	} else {
 
@@ -51,7 +50,7 @@ func NovedadShow(w http.ResponseWriter, r *http.Request) {
 
 		var novedad structNovedad.Novedad //Con &var --> lo que devuelve el metodo se le asigna a la var
 
-		db := obtenerDB(tokenAutenticacion)
+		db := apiclientconexionbd.ObtenerDB(tokenAutenticacion)
 		automigrateTablasPrivadas(db)
 		defer db.Close()
 
@@ -68,10 +67,10 @@ func NovedadShow(w http.ResponseWriter, r *http.Request) {
 
 func NovedadAdd(w http.ResponseWriter, r *http.Request) {
 
-	tokenAutenticacion, tokenError := checkTokenValido(r)
+	tokenAutenticacion, tokenError := apiclientautenticacion.CheckTokenValido(r)
 
 	if tokenError != nil {
-		errorToken(w, tokenError)
+		apiclientautenticacion.ErrorToken(w, tokenError)
 		return
 	} else {
 
@@ -86,7 +85,7 @@ func NovedadAdd(w http.ResponseWriter, r *http.Request) {
 
 		defer r.Body.Close()
 
-		db := obtenerDB(tokenAutenticacion)
+		db := apiclientconexionbd.ObtenerDB(tokenAutenticacion)
 		automigrateTablasPrivadas(db)
 		defer db.Close()
 
@@ -101,11 +100,11 @@ func NovedadAdd(w http.ResponseWriter, r *http.Request) {
 
 func NovedadUpdate(w http.ResponseWriter, r *http.Request) {
 
-	tokenAutenticacion, tokenError := checkTokenValido(r)
+	tokenAutenticacion, tokenError := apiclientautenticacion.CheckTokenValido(r)
 
 	if tokenError != nil {
 
-		errorToken(w, tokenError)
+		apiclientautenticacion.ErrorToken(w, tokenError)
 		return
 	} else {
 
@@ -135,7 +134,7 @@ func NovedadUpdate(w http.ResponseWriter, r *http.Request) {
 
 			novedad_data.ID = p_novedadid
 
-			db := obtenerDB(tokenAutenticacion)
+			db := apiclientconexionbd.ObtenerDB(tokenAutenticacion)
 			automigrateTablasPrivadas(db)
 			defer db.Close()
 
@@ -156,11 +155,11 @@ func NovedadUpdate(w http.ResponseWriter, r *http.Request) {
 
 func NovedadRemove(w http.ResponseWriter, r *http.Request) {
 
-	tokenAutenticacion, tokenError := checkTokenValido(r)
+	tokenAutenticacion, tokenError := apiclientautenticacion.CheckTokenValido(r)
 
 	if tokenError != nil {
 
-		errorToken(w, tokenError)
+		apiclientautenticacion.ErrorToken(w, tokenError)
 		return
 	} else {
 
@@ -168,7 +167,7 @@ func NovedadRemove(w http.ResponseWriter, r *http.Request) {
 		params := mux.Vars(r)
 		novedad_id := params["id"]
 
-		db := obtenerDB(tokenAutenticacion)
+		db := apiclientconexionbd.ObtenerDB(tokenAutenticacion)
 		automigrateTablasPrivadas(db)
 		defer db.Close()
 
@@ -184,57 +183,9 @@ func NovedadRemove(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func obtenerDB(tokenAutenticacion *publico.Security) *gorm.DB {
-
-	token := *tokenAutenticacion
-	tenant := token.Tenant
-
-	return conexionBD.ConnectBD(tenant)
-
-}
-
 func automigrateTablasPrivadas(db *gorm.DB) {
 
 	//para actualizar tablas...agrega columnas e indices, pero no elimina
 	db.AutoMigrate(&structNovedad.Novedad{})
 
-}
-
-func errorToken(w http.ResponseWriter, tokenError *publico.Error) {
-	errorToken := *tokenError
-	framework.RespondError(w, errorToken.ErrorCodigo, errorToken.ErrorNombre)
-
-}
-
-func checkTokenValido(r *http.Request) (*publico.Security, *publico.Error) {
-
-	var tokenAutenticacion *publico.Security
-	var tokenError *publico.Error
-
-	url := "http://localhost:8081/check-token"
-
-	req, _ := http.NewRequest("GET", url, nil)
-
-	header := r.Header.Get("Authorization")
-
-	req.Header.Add("Authorization", header)
-
-	res, _ := http.DefaultClient.Do(req)
-
-	defer res.Body.Close()
-	body, _ := ioutil.ReadAll(res.Body)
-
-	if res.StatusCode != http.StatusBadRequest {
-
-		// tokenAutenticacion = &(TokenAutenticacion{})
-		tokenAutenticacion = new(publico.Security)
-		json.Unmarshal([]byte(string(body)), tokenAutenticacion)
-
-	} else {
-		tokenError = new(publico.Error)
-		json.Unmarshal([]byte(string(body)), tokenError)
-
-	}
-
-	return tokenAutenticacion, tokenError
 }
--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/xubiosueldos/novedad
 require (
 	github.com/gorilla/mux v1.7.1
 	github.com/jinzhu/gorm v1.9.8
-	github.com/xubiosueldos/autenticacion v0.0.0-20190502190535-473a26c168c7
-	github.com/xubiosueldos/conexionBD v0.0.0-20190503185834-fb2a794e1c8e
+	github.com/xubiosueldos/autenticacion v0.0.0-20190515151453-a07720b1076f
+	github.com/xubiosueldos/conexionBD v0.0.0-20190515140813-fc0ed0d791d3
 	github.com/xubiosueldos/framework v0.0.0-20190514130847-a392b8b7bf9b
 )
