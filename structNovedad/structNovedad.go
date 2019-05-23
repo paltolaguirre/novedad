289b87c4b2c20ca66662b279c682233f1762af8e
Paula Altolaguirre <paltolaguirre@finnegans.com.ar>
2019-05-23 09:43:58 -0300

    se agregaron nuevos campos al struct de novedad

--- structNovedad/structNovedad.go
+++ structNovedad/structNovedad.go
@@ -1,11 +1,24 @@
 package structNovedad
 
-import "github.com/xubiosueldos/conexionBD/structGormModel"
+import (
+	"time"
+
+	"github.com/xubiosueldos/concepto/structConcepto"
+	"github.com/xubiosueldos/conexionBD/structGormModel"
+	"github.com/xubiosueldos/legajo/structLegajo"
+)
 
 type Novedad struct {
 	structGormModel.GormModel
-	Nombre      string `json:"nombre"`
-	Codigo      string `json:"codigo"`
-	Descripcion string `json:"descripcion"`
-	Activo      int    `json:"activo"`
+	Nombre      string                   `json:"nombre"`
+	Codigo      string                   `json:"codigo"`
+	Descripcion string                   `json:"descripcion"`
+	Activo      int                      `json:"activo"`
+	Importe     float32                  `json:"importe"`
+	Cantidad    int                      `json:"cantidad"`
+	Fecha       time.Time                `json:"fecha"`
+	Legajo      *structLegajo.Legajo     `json:"legajo" gorm:"ForeignKey:Legajoid;association_foreignkey:ID;association_autoupdate:false"`
+	Legajoid    *int                     `json:"legajoid" sql:"type:int REFERENCES Legajo(ID)"`
+	Concepto    *structConcepto.Concepto `json:"concepto" gorm:"ForeignKey:Conceptoid;association_foreignkey:ID;association_autoupdate:false"`
+	Conceptoid  *int                     `json:"conceptoid" sql:"type:int REFERENCES Concepto(ID)"`
 }
