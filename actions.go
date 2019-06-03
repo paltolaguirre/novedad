a24bd59c187ac1526e603202ffee0cb56a33120c
Paula Altolaguirre <paltolaguirre@finnegans.com.ar>
2019-06-03 12:31:16 -0300

    Puerto equivocado: pasaba el puerto de novedad y no el de concepto

--- actions.go
+++ actions.go
@@ -72,7 +72,7 @@ func obtenerConcepto(conceptoid int, r *http.Request) *structConcepto.Concepto {
 
 	config := configuracion.GetInstance()
 
-	url := configuracion.GetUrlMicroservicio(config.Puertomicroservicionovedad) + "concepto/conceptos/" + strconv.Itoa(conceptoid)
+	url := configuracion.GetUrlMicroservicio(config.Puertomicroservicioconcepto) + "concepto/conceptos/" + strconv.Itoa(conceptoid)
 
 	//url := "http://localhost:8084/conceptos/" + strconv.Itoa(conceptoid)
 
