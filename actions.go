df1825a9602d76762d3fc11c35c0f91893094e30
Paula Altolaguirre <paltolaguirre@finnegans.com.ar>
2019-06-11 15:14:33 -0300

    nueva funcion para cerrar las conexiones

--- actions.go
+++ actions.go
@@ -32,7 +32,8 @@ func NovedadList(w http.ResponseWriter, r *http.Request) {
 
 		db := apiclientconexionbd.ObtenerDB(tenant, nombreMicroservicio, versionMicroservicio, AutomigrateTablasPrivadas)
 
-		defer db.Close()
+		//defer db.Close()
+		defer apiclientconexionbd.CerrarDB(db)
 
 		var novedades []structNovedad.Novedad
 
@@ -58,7 +59,8 @@ func NovedadShow(w http.ResponseWriter, r *http.Request) {
 
 		db := apiclientconexionbd.ObtenerDB(tenant, nombreMicroservicio, versionMicroservicio, AutomigrateTablasPrivadas)
 
-		defer db.Close()
+		//defer db.Close()
+		defer apiclientconexionbd.CerrarDB(db)
 
 		//gorm:auto_preload se usa para que complete todos los struct con su informacion
 		if err := db.Set("gorm:auto_preload", true).First(&novedad, "id = ?", novedad_id).Error; gorm.IsRecordNotFoundError(err) {
@@ -139,7 +141,8 @@ func NovedadAdd(w http.ResponseWriter, r *http.Request) {
 
 		db := apiclientconexionbd.ObtenerDB(tenant, nombreMicroservicio, versionMicroservicio, AutomigrateTablasPrivadas)
 
-		defer db.Close()
+		//defer db.Close()
+		defer apiclientconexionbd.CerrarDB(db)
 
 		if err := db.Create(&novedad_data).Error; err != nil {
 			framework.RespondError(w, http.StatusInternalServerError, err.Error())
@@ -186,7 +189,8 @@ func NovedadUpdate(w http.ResponseWriter, r *http.Request) {
 
 			db := apiclientconexionbd.ObtenerDB(tenant, nombreMicroservicio, versionMicroservicio, AutomigrateTablasPrivadas)
 
-			defer db.Close()
+			//defer db.Close()
+			defer apiclientconexionbd.CerrarDB(db)
 
 			if err := db.Save(&novedad_data).Error; err != nil {
 				framework.RespondError(w, http.StatusInternalServerError, err.Error())
@@ -217,7 +221,8 @@ func NovedadRemove(w http.ResponseWriter, r *http.Request) {
 
 		db := apiclientconexionbd.ObtenerDB(tenant, nombreMicroservicio, versionMicroservicio, AutomigrateTablasPrivadas)
 
-		defer db.Close()
+		//defer db.Close()
+		defer apiclientconexionbd.CerrarDB(db)
 
 		//--Borrado Fisico
 		if err := db.Unscoped().Where("id = ?", novedad_id).Delete(structNovedad.Novedad{}).Error; err != nil {
